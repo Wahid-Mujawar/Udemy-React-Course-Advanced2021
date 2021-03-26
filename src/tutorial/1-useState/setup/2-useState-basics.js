26264b0cc99485370ed94523045f5970586e7b0f
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-25 22:58:47 -0600

    test

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -2,9 +2,17 @@ import React, { useState } from 'react';
 
 const UseStateBasics = () => {
   const [text, setText] =useState('Random Title')
+
   const handleClick = () =>{
-    setText('Change To New Tilte')
-  }
+    if(text === 'Random Title')
+    {
+      setText('Change To New Tilte')
+    }
+    else{
+      setText('Random Title')
+    }
+  };
+
   return (
     <React.Fragment>
       <h2>{text}</h2>
