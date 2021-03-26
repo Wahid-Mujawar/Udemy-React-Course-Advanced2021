6f3a161b05e2d1fc58c179155c3d7644c66e6e52
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-25 22:55:14 -0600

    test

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -9,6 +9,7 @@ const UseStateBasics = () => {
     <React.Fragment>
       <h2>{text}</h2>
       <button type="button" className="btn" onClick={handleClick}>
+        Change Title</button>
     </React.Fragment>
   )
 };
