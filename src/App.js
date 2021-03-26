21c0ccfe54d0de6cd0599985ee8971f83df1bcd3
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-25 19:59:52 -0600

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <h2>Udemy React Course Advanced</h2>
     </div>
   )
 }
