4b59e3fb5683e5de4ca08275ce59435ca1f72888
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-25 22:51:42 -0600

    test

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return 
+  const [text, setText] =useState('Random Title')
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button type="button" className="btn" onClick={handleClick}
+        Change Title></button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;
