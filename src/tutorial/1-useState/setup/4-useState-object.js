36dd4507b66de7ce7344d888c24ff92d47c9649d
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-27 15:48:15 -0600

    test

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -7,11 +7,15 @@ const UseStateObject = () => {
     message:'Engineer 2020',
   })
 
+  const changeMessage =() =>{
+    setPerson({message: 'Computer Science'})
+  }
   return (
     <React.Fragment>
       <h2>{person.name}</h2>
       <h2>{person.age}</h2>
       <h2>{person.message}</h2>
+      <button type="button" className="btn" onClick={changeMessage}>Change Message</button>
     </React.Fragment>
   )
 };
