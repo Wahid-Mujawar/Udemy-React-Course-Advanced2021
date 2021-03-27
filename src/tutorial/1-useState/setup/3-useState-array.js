9e6c11ab8e72bf416b74770bb27da4458c7c822d
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-27 12:09:18 -0600

    test

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople]=useState(data);
+
+  return (
+    <React.Fragment>
+      {people.map((person)  => {
+        console.log(person)
+        return 'Array'
+      })}
+    </React.Fragment>
+  )
 };
 
 export default UseStateArray;
