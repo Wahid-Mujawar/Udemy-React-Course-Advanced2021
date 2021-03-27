4f65735f1c82697a0005ca1b2f5d599f31132db2
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-27 15:45:26 -0600

    test

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const[person, setPerson]=useState({
+    name: 'Wahid',
+    age:'22',
+    message:'Engineer 2020',
+  })
+
+  return (
+    <React.Fragment>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.message}</h2>
+    </React.Fragment>
+  )
 };
 
 export default UseStateObject;
