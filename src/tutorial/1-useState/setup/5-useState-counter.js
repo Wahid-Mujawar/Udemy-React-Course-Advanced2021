d7e77f539b0693122ef173b522941efe366affbb
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-27 19:03:05 -0600

    test

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,15 +3,19 @@ import React, { useState } from 'react';
 const UseStateCounter = () => {
   const [value, setValue]=useState(0);
 
+  const reset = () =>{
+    setValue(0);
+  }
+
   return (
     <React.Fragment>
       <section style={{margin:'4rem 0'}}>
         <h2>Regular Counter</h2>
         <h2>{value}</h2>
 
-        <button className="btn" onClick={() => setValue(value-1)}>Increase</button>
+        <button className="btn" onClick={() => setValue(value+1)}>Increase</button>
         <button className="btn" onClick={reset}>Reset</button>
-        <button className="btn" onClick={() => setValue(value+1)}>Decrease</button>
+        <button className="btn" onClick={() => setValue(value-1)}>Decrease</button>
 
       </section>
     </React.Fragment>
