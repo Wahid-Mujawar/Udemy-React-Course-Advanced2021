b061908f4c686a3b8cb494a3dee85c1ccf684b51
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-27 19:34:44 -0600

    test

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -5,18 +5,28 @@ const UseStateCounter = () => {
 
   const reset = () =>{
     setValue(0);
-  }
+  };
 
+  const complexIncrease = () => {
+    setTimeout (()=>{
+      setValue(value+1);
+    });
+  }
+  
   return (
     <React.Fragment>
       <section style={{margin:'4rem 0'}}>
         <h2>Regular Counter</h2>
         <h2>{value}</h2>
 
-        <button className="btn" onClick={() => setValue(value+1)}>Increase</button>
-        <button className="btn" onClick={reset}>Reset</button>
-        <button className="btn" onClick={() => setValue(value-1)}>Decrease</button>
-
+        <button className="btn"
+        onClick={() => setValue(value+1)}>Increase</button>
+        <button className="btn" 
+        onClick={reset}>Reset</button>
+        <button className="btn" 
+        onClick={() => setValue(value-1)}>Decrease</button>
+        <button type="button" className="btn"
+        onClick={{complexIncrease}}>Increase Delay</button>
       </section>
     </React.Fragment>
   )
