9b9aebd9f943dd4d187fa4e0074091ce7520e6f9
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-04-16 00:03:18 +0530

    test

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+
+  const [show, setShow] = useState(false);
+
+  return(
+    <React.Fragment>
+      <button className="btn" onClick = {() =>setShow(!show)}>
+        Show/Hide
+      </button>
+    </React.Fragment>
+  )
 };
 
 export default ShowHide;
